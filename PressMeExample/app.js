39c75405812626b630b29c62ad6a57f8518cee95
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-08-08 17:31:12 +0800

    :fire: Import package in example

--- PressMeExample/app.js
+++ PressMeExample/app.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
-
-import PressMeButton from './PressMeButton'
+import PressMeButton from 'react-native-press-me-button'
 
 export default class PressMeExample extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <PressMeButton title="Press me!" style={{ height: 50, width: 240 }} />
+        <PressMeButton
+          title="Press me!"
+          style={{ height: 50, width: 240 }}
+          bottomRadius={3}
+          pulse={false}
+        />
       </View>
     )
   }
